2fac766e84e0732c7379f0cec86954a10e25526b
masinyax <mimouse2@gmail.com>
2026-04-30 04:19:05 +0700

    Fix: redirect home page to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { db, auth } from "@/lib/firebase";
-import { collection, addDoc, serverTimestamp, setDoc, doc } from "firebase/firestore"; // เพิ่ม setDoc, doc
 import { redirect } from "next/navigation";
 
 export default function Home() {
